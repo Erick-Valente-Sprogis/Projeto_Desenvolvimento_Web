e878e9879e787c71dd5b9ebde1e4926e35fb5205
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-09-02 09:57:38 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,22 @@
-window.alert( "Hello World!" );
+// Referências aos elementos
+const openPopup = document.getElementById('openPopup');
+const popup = document.getElementById('popup');
+const closePopup = document.getElementById('closePopup');
+
+// Abre o pop-up
+openPopup.addEventListener('click', function (event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    popup.style.display = 'flex'; // Mostra o pop-up
+});
+
+// Fecha o pop-up
+closePopup.addEventListener('click', function () {
+    popup.style.display = 'none'; // Esconde o pop-up
+});
+
+// Fecha o pop-up quando clicar fora dele
+window.addEventListener('click', function (event) {
+    if (event.target === popup) {
+        popup.style.display = 'none'; // Esconde o pop-up
+    }
+});
