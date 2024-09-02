5700eed7fae8fb2dde96b433d29d54bd647d4bea
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-09-02 11:12:16 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,22 +1,24 @@
-// Referências aos elementos
-const openPopup = document.getElementById('openPopup');
-const popup = document.getElementById('popup');
-const closePopup = document.getElementById('closePopup');
+// Seleciona todos os elementos com a classe "openPopup"
+const openPopups = document.querySelectorAll( '.openPopup' );
+const popup = document.getElementById( 'popup' );
+const closePopup = document.getElementById( 'closePopup' );
 
-// Abre o pop-up
-openPopup.addEventListener('click', function (event) {
-    event.preventDefault(); // Previne o comportamento padrão do link
-    popup.style.display = 'flex'; // Mostra o pop-up
-});
+// Adiciona um evento de clique para cada elemento
+openPopups.forEach( function ( openPopup ) {
+    openPopup.addEventListener( 'click', function ( event ) {
+        event.preventDefault(); // Previne o comportamento padrão do link
+        popup.style.display = 'flex'; // Mostra o pop-up
+    } );
+} );
 
 // Fecha o pop-up
-closePopup.addEventListener('click', function () {
+closePopup.addEventListener( 'click', function () {
     popup.style.display = 'none'; // Esconde o pop-up
-});
+} );
 
 // Fecha o pop-up quando clicar fora dele
-window.addEventListener('click', function (event) {
-    if (event.target === popup) {
+window.addEventListener( 'click', function ( event ) {
+    if ( event.target === popup ) {
         popup.style.display = 'none'; // Esconde o pop-up
     }
-});
+} );
