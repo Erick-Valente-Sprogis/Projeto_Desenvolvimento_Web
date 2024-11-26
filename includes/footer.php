e233f17ff59d5d8b0349baff356b621e04f21178
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-11-26 16:39:45 -0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>&copy; 2024 Joined Hands. Todos os direitos reservados.</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
