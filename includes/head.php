5421226ca84167fbfdfde6feff34b79a9766f0d9
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-11-26 16:39:29 -0300

    Create head.php

---
+++ includes/head.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="imgs/Joined Hands_Logo.png" sizes="32x32" type="image/png">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/temqteremtudo.css">
+    <link rel="stylesheet" href="css/contato.css">
+    <link rel="stylesheet" href="css/popupLogin.css">
+    <link rel="stylesheet" href="css/pop_up.css">
+    <link rel="stylesheet" href="css/modules/sangue.css">
+    <link rel="stylesheet" href="css/modules/monetaria.css">
+    <link rel="stylesheet" href="css/modules/alimento.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Joined Hands - Faça sua doação HOJE!</title>
+</head>
