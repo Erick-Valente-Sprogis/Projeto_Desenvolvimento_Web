99cca84b06fd8cbc2354077bd314d2ce4b540480
Erick Valente Sprogis <103703957+Erick-Valente-Sprogis@users.noreply.github.com>
2024-11-27 17:22:30 -0300

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -12,5 +12,8 @@
     <link rel="stylesheet" href="css/modules/alimento.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-srIYreWPQG6l6oy0vr4Rsm0mDJbzYaeFZhD62h8VZ/xRxD8rCf7HGGjJhuEIQxmK" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-wEa6ROwDFHOkHu5wN/3BOuRNo9uMYH7V3Ifx26DE85ZSkAC/QJ6Z6VwvkwrIk49T" crossorigin="anonymous"></script>
+
     <title>Joined Hands - Faça sua doação HOJE!</title>
 </head>
